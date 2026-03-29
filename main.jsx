7a20c1c15592619d89cc62ecb6219841f99ceaae
Chad H Glaser <codingwithchad@gmail.com>
2026-03-28 19:14:58 -0700

    Initialize main.jsx with ReactDOM rendering

---
+++ main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import OutpostHQ from './landingpage.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <OutpostHQ />
+  </React.StrictMode>
+)
